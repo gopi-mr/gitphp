e67ab50f0410213cc3c216935f94b26cee166262
gopi-mr <gopinathmr21@gmail.com>
2013-10-19 09:14:07 -0700

    After Menu Add

--- index.php
+++ index.php
@@ -5,48 +5,17 @@
 </head>
 
 <body>
-
-<h1>This is My Website</h1>
-<div id="header">   <!--Header Div-->
-	<div id="logodiv">
-		<a href="index.php" >
-		<img src="files/logo2.png" alt="LOGO" id="imglogo"></img> <span id="bylmec" >By LMEC </span>
-		</a> 
-	</div>
-</div>
-
-
-<div id="menudiv" >    <!-- Menu Div-->
+<header>
+	<h1>This is Gopi's Website</h1>
+	<nav>
+		<ul>
+			<li><a href="#" >Home</a></li>
+			<li><a href="#" >College</a></li>
+			<li><a href="#" >Student</a></li>
 		
-	<table cellspacing="5">
-		<tr>
-			<td>
-				<img src="" alt="" height="15px" width="15px">
-				<a href="index.php" class ="menuname">Home</a>
-			
-			</td>
-			<td>	
-				<img src="" alt=""  height="15px" width="15px">
-				<a href="events.php" class ="menuname">Events</a>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<img src="" alt=""  height="15px" width="15px">
-				<a href="joinEvents.php" class ="menuname">Join Events</a>
-			</td>
-			<td>
-				<img src="" alt=""  height="15px" width="15px">
-				<a href="contact.php" class ="menuname">Contact</a>
-			</td>
-		</tr>
-	</table>
-		
-</div>
-
-
-
-
+		</ul>
+	</nav>
+</header>
 </body>
 
 </html>
