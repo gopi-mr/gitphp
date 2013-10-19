b2ecbc31157f130327ee906f465c3d3c09c9e3bd
gopi-mr <gopinathmr21@gmail.com>
2013-10-19 09:09:46 -0700

    add index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Java script Test using git</title>
+	<link rel="stylesheet" href="files/style.css"/>
+	<script  src="files/jquery.js"></script>
+	<script src="files/myscript.js" ></script>
+</head>
+
+<body>
+
+<h1>Welcome to Github Directory</h1>
+<div id="header">   <!--Header Div-->
+	<div id="logodiv">
+		<a href="index.php" >
+		<img src="files/logo2.png" alt="LOGO" id="imglogo"></img> <span id="bylmec" >By LMEC </span>
+		</a> 
+	</div>
+</div>
+
+
+<div id="menudiv" >    <!-- Menu Div-->
+		
+	<table cellspacing="5">
+		<tr>
+			<td>
+				<img src="" alt="" height="15px" width="15px">
+				<a href="index.php" class ="menuname">Home</a>
+			
+			</td>
+			<td>	
+				<img src="" alt=""  height="15px" width="15px">
+				<a href="events.php" class ="menuname">Events</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<img src="" alt=""  height="15px" width="15px">
+				<a href="joinEvents.php" class ="menuname">Join Events</a>
+			</td>
+			<td>
+				<img src="" alt=""  height="15px" width="15px">
+				<a href="contact.php" class ="menuname">Contact</a>
+			</td>
+		</tr>
+	</table>
+		
+</div>
+
+
+<div id="buttondiv" >   <!--Button Div-->	
+		<b>Show/hide Menu</b>
+</div>
+
+</body>
+
+</html>
